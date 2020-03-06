fa11b867e848adb1e71d2fac58c6ca0f88547e01
mrewo <konrad.kihan@gmail.com>
2020-03-06 20:30:53 +0100

    Updated data

--- parse_data.py
+++ parse_data.py
@@ -3,12 +3,12 @@ import csv
 with open('genome_zeeshan_usmani.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
-# searches for RSID that is avaliable in other databases (the RS one)
+    # searches for RSID that is avaliable in other databases (the RS one)
+    gene_id = []
     def rsid_finder():
-        gene_id = []
         for line in csv_reader:
-            if line["# rsid"][0] == "r":
-                gene_id.append(line["# rsid"])  # ERROR - LIST ALWAYS EMPTY
+            if line["rsid"][0] == "r":
+                gene_id.append(line["rsid"])  # list that contains all RSIDs
 
         return gene_id
 
@@ -16,4 +16,3 @@ with open('genome_zeeshan_usmani.csv', 'r') as csv_file:
 
     gene_id = rsid_finder()
 
-    print(gene_id)
