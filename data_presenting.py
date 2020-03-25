d5a3c88a881713b54116561fff17812d3ad9a209
OlhaBabicheva <57872493+OlhaBabicheva@users.noreply.github.com>
2020-03-25 14:40:30 +0100

    Add files via upload

--- data_presenting.py
+++ data_presenting.py
@@ -70,6 +70,7 @@ postioner = 0
 is_appeard = []
 try:
     filetoread = input("Enter name of your dictionary file")
+    resultfile = open('wynik.txt', 'a+')
     with open(filetoread, "r") as dictionary:
         csv_reader = csv.reader(dictionary)
 
@@ -88,6 +89,8 @@ try:
                 # thanks to this IF lines dont multiply
                 if line[1] not in is_appeard:
                     print(line[1], corel[corelation_index], issue[issue_index])
+                    result = line[1], corel[corelation_index], issue[issue_index]
+                    resultfile.write(str(result))
                     is_appeard.append(line[1])
                 else:
                     pass
